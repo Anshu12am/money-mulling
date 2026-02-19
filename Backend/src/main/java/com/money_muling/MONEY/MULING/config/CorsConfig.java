02f77fcb76554a24c4a493fda6bdca57f56cf691
Anshu12am <another.hell.11@gmail.com>
2026-02-20 04:42:36 +0530

    third commit

--- Backend/src/main/java/com/money_muling/MONEY/MULING/config/CorsConfig.java
+++ Backend/src/main/java/com/money_muling/MONEY/MULING/config/CorsConfig.java
@@ -15,11 +15,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:5173") // <-- updated port
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
     }
 }
+
 
